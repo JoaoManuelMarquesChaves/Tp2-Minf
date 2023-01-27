58c30813683525283a34a88faf05a582f02e3b1a
JoaoManuelMarquesChaves <119414658+JoaoManuelMarquesChaves@users.noreply.github.com>
2023-01-27 21:40:21 +0100

    Add files via upload

---
+++ Mc32gest_RS232.c
@@ -0,0 +1,300 @@
+// Mc32Gest_RS232.C
+// Canevas manipulatio TP2 RS232 SLO2 2017-18
+// Fonctions d'émission et de réception des message
+// CHR 20.12.2016 ajout traitement int error
+// CHR 22.12.2016 evolution des marquers observation int Usart
+// SCA 03.01.2018 nettoyé réponse interrupt pour ne laisser que les 3 ifs
+
+#include <xc.h>
+#include <sys/attribs.h>
+#include "system_definitions.h"
+#include <GenericTypeDefs.h>
+#include "app.h"
+#include "GesFifoTh32.h"
+#include "Mc32gest_RS232.h"
+#include "gestPWM.h"
+#include "Mc32CalCrc16.h"
+
+
+typedef union {
+        uint16_t val;
+        struct {uint8_t lsb;
+                uint8_t msb;} shl;
+} U_manip16;
+
+
+// Definition pour les messages
+#define MESS_SIZE  5
+// avec int8_t besoin -86 au lieu de 0xAA
+#define STX_code  (-86)
+
+// Structure décrivant le message
+typedef struct {
+    int8_t Start;
+    int8_t  Speed;
+    int8_t  Angle;
+    int8_t MsbCrc;
+    int8_t LsbCrc;
+} StruMess;
+
+
+// Struct pour émission des messages
+StruMess TxMess;
+// Struct pour réception des messages
+StruMess RxMess;
+
+// Declaration des FIFO pour réception et émission
+#define FIFO_RX_SIZE ( (4*MESS_SIZE) + 1)  // 4 messages
+#define FIFO_TX_SIZE ( (4*MESS_SIZE) + 1)  // 4 messages
+
+int8_t fifoRX[FIFO_RX_SIZE];
+// Declaration du descripteur du FIFO de réception
+S_fifo descrFifoRX;
+
+
+int8_t fifoTX[FIFO_TX_SIZE];
+// Declaration du descripteur du FIFO d'émission
+S_fifo descrFifoTX;
+
+
+// Initialisation de la communication sérielle
+void InitFifoComm(void)
+{    
+    // Initialisation du fifo de réception
+    InitFifo ( &descrFifoRX, FIFO_RX_SIZE, fifoRX, 0 );
+    // Initialisation du fifo d'émission
+    InitFifo ( &descrFifoTX, FIFO_TX_SIZE, fifoTX, 0 );
+    
+    // Init RTS 
+    RS232_RTS = 1;   // interdit émission par l'autre
+   
+} // InitComm
+
+int GetMessage(S_pwmSettings *pData)
+{
+    static uint8_t GetSituation =  0 ;
+    static uint16_t ValCrc;       // en cas d'abandon en cours
+    uint8_t  EndMess;
+    uint8_t  NbCharToRead = 0;
+    int MessReady = 0;     // indique validité message reçu
+    // Détermine le nombre de caractères à lire
+    NbCharToRead = GetReadSize( &descrFifoRX);
+    // Si >= taille message alors traite
+    if(NbCharToRead >= MESS_SIZE)  
+    {
+        EndMess = 0;
+        while( (NbCharToRead >= 1) && ( EndMess == 0) ) 
+        {
+            // Traitement selon état du message
+            switch(GetSituation)  
+            {
+                case 0: // control et recuperation start
+                    GetCharFromFifo( &descrFifoRX, &RxMess.Start );
+                    NbCharToRead--;
+                    if( RxMess.Start == STX_code) 
+                    {
+                        ValCrc = 0xFFFF;
+                        ValCrc = updateCRC16(ValCrc, RxMess.Start);
+                        GetSituation = 1;
+                    } 
+                    break;
+                case 1: // recuperation speed
+                    GetCharFromFifo( &descrFifoRX, &RxMess.Speed );
+                    NbCharToRead--;
+                    ValCrc = updateCRC16(ValCrc, RxMess.Speed);
+                    GetSituation = 2;
+                    break;
+                case 2: // recuperation Angle
+                    GetCharFromFifo( &descrFifoRX, &RxMess.Angle );
+                    NbCharToRead--;
+                    ValCrc = updateCRC16(ValCrc, RxMess.Angle);
+                    GetSituation = 3;
+                    break;
+                case 3: // recuperation Crc Msb
+                    GetCharFromFifo( &descrFifoRX, &RxMess.MsbCrc );
+                    NbCharToRead--;
+                    ValCrc = updateCRC16(ValCrc, RxMess.MsbCrc);
+                    GetSituation = 4;
+                    break;
+                case 4: // recuperation Crc Lsb
+                    GetCharFromFifo( &descrFifoRX, &RxMess.LsbCrc );
+                    NbCharToRead--;
+                    ValCrc = updateCRC16(ValCrc, RxMess.LsbCrc);
+                    GetSituation = 5;
+                    break;                   
+                case 5: // control Crc
+                    if(ValCrc == 0 ) 
+                    {
+                        // ok -> envoie data
+                        pData->SpeedSetting = RxMess.Speed;
+                        pData->AngleSetting = RxMess.Angle;
+                        MessReady = 1;
+                    }
+                    else 
+                    {
+                            BSP_LEDToggle(BSP_LED_6); // test Crc nop
+                    }
+                    GetSituation = 0;
+                    EndMess = 1;   // pour ne traiter qu'un seul message à la fois
+                    break;
+            } // end switch
+        } // end while
+    } 
+    // Gestion control de flux de la réception
+    // 12 correspond à 2 paquets de 6 (3/4 RxBuffer de 8)
+    if(GetWriteSpace ( &descrFifoRX) >= 12) 
+    {
+        // Autorise émission par l'autre
+        RS232_RTS = 0;
+    }
+    return MessReady;
+} // End GetMessage
+
+void SendMessage(S_pwmSettings *pData)
+{
+    int32_t freeSize;
+    uint16_t ValCrc16 = 0XFFFF; 
+    U_manip16 tmpCrc;
+    // Test si place Pour écrire 1 message
+    freeSize = GetWriteSpace ( &descrFifoTX); 
+    // Traitement émission à introduire ICI
+    if(freeSize >= (MESS_SIZE))
+    {       
+        //compose le message
+        TxMess.Start = 0XAA; // start
+        ValCrc16 = updateCRC16(ValCrc16, TxMess.Start );
+        // Traitement data
+        TxMess.Speed = pData->SpeedSetting ;
+        ValCrc16 = updateCRC16(ValCrc16, TxMess.Speed );
+        // Traitement data
+        TxMess.Angle = pData->AngleSetting;
+        ValCrc16 = updateCRC16(ValCrc16, TxMess.Angle );
+        // Traitement CRC
+        tmpCrc.val = ValCrc16;
+        TxMess.MsbCrc = tmpCrc.shl.msb;
+        TxMess.LsbCrc = tmpCrc.shl.lsb;
+        // Dépose le message dans le fifo
+        PutCharInFifo (&descrFifoTX, TxMess.Start);
+        PutCharInFifo (&descrFifoTX, TxMess.Speed);
+        PutCharInFifo (&descrFifoTX, TxMess.Angle);
+        PutCharInFifo (&descrFifoTX, TxMess.MsbCrc);
+        PutCharInFifo (&descrFifoTX, TxMess.LsbCrc);
+    }
+    
+    // Gestion du controle de flux
+    // si on a un caractère à envoyer et que CTS = 0
+    freeSize = GetReadSize(&descrFifoTX);
+    if ((RS232_CTS == 0) && (freeSize >0))
+    {
+        // Autorise int émission    
+        PLIB_INT_SourceEnable(INT_ID_0, INT_SOURCE_USART_1_TRANSMIT);                
+    }
+}
+
+// Interruption USART1
+// !!!!!!!!
+// Attention ne pas oublier de supprimer la réponse générée dans system_interrupt
+// !!!!!!!!
+ void __ISR(_UART_1_VECTOR, ipl5AUTO) _IntHandlerDrvUsartInstance0(void)
+{
+    USART_ERROR UsartStatus;    
+    int8_t i_cts;
+    uint8_t TXsize,freeSize;
+    BOOL TxBuffFull;
+    int8_t c;
+
+    // Marque début interruption avec Led3
+    LED3_W = 1;
+    
+    // Is this an Error interrupt ?
+    if ( PLIB_INT_SourceFlagGet(INT_ID_0, INT_SOURCE_USART_1_ERROR) &&
+                 PLIB_INT_SourceIsEnabled(INT_ID_0, INT_SOURCE_USART_1_ERROR) ) {
+        /* Clear pending interrupt */
+        PLIB_INT_SourceFlagClear(INT_ID_0, INT_SOURCE_USART_1_ERROR);
+        // Traitement de l'erreur à la réception.
+    }
+   
+    // Is this an RX interrupt ?
+    if ( PLIB_INT_SourceFlagGet(INT_ID_0, INT_SOURCE_USART_1_RECEIVE) &&
+                 PLIB_INT_SourceIsEnabled(INT_ID_0, INT_SOURCE_USART_1_RECEIVE) ) {
+
+        // Oui Test si erreur parité ou overrun
+        UsartStatus = PLIB_USART_ErrorsGet(USART_ID_1);
+
+        if ( (UsartStatus & (USART_ERROR_PARITY |
+                             USART_ERROR_FRAMING | USART_ERROR_RECEIVER_OVERRUN)) == 0) {
+
+            
+            // Lecture des caractères depuis le buffer HW -> fifo SW
+			// pour savoir s'il y a une data dans le buffer HW RX 
+            while (PLIB_USART_ReceiverDataIsAvailable(USART_ID_1))
+            {
+                // Traitement RX à faire ICI
+                // Lecture des caractères depuis le buffer HW -> fifo SW
+                c = PLIB_USART_ReceiverByteReceive(USART_ID_1);
+                PutCharInFifo ( &descrFifoRX, c);
+            }
+            
+            LED4_W = !LED4_R; // Toggle Led4
+            // buffer is empty, clear interrupt flag
+            PLIB_INT_SourceFlagClear(INT_ID_0, INT_SOURCE_USART_1_RECEIVE);
+        } else {
+            // Suppression des erreurs
+            // La lecture des erreurs les efface sauf pour overrun
+            if ( (UsartStatus & USART_ERROR_RECEIVER_OVERRUN) == USART_ERROR_RECEIVER_OVERRUN) {
+                   PLIB_USART_ReceiverOverrunErrorClear(USART_ID_1);
+            }
+        }
+
+        
+        // Traitement controle de flux reception à faire ICI
+        // Gerer sortie RS232_RTS en fonction de place dispo dans fifo reception
+        freeSize = GetWriteSpace ( &descrFifoRX);
+            if (freeSize <= 6) // a cause d'un int pour 6 char
+            {
+            // Demande de ne plus émettre
+                RS232_RTS = 1;              
+            }
+
+        
+    } // end if RX
+
+    
+    // Is this an TX interrupt ?
+    if ( PLIB_INT_SourceFlagGet(INT_ID_0, INT_SOURCE_USART_1_TRANSMIT) &&
+                 PLIB_INT_SourceIsEnabled(INT_ID_0, INT_SOURCE_USART_1_TRANSMIT) ) {
+
+        
+
+            
+        // Avant d'émettre, on vérifie 3 conditions :
+        //  Si CTS = 0 autorisation d'émettre (entrée RS232_CTS)
+        //  S'il y a un caratères à émettre dans le fifo
+        //  S'il y a de la place dans le buffer d'émission
+       i_cts = RS232_CTS;
+       TXsize = GetReadSize (&descrFifoTX);
+       TxBuffFull = PLIB_USART_TransmitterBufferIsFull(USART_ID_1);
+       
+       if ( (i_cts == 0) && ( TXsize > 0 ) && TxBuffFull==false )
+       {
+           do{
+                // Traitement TX à faire ICI
+                // Envoi des caractères depuis le fifo SW -> buffer HW
+                GetCharFromFifo(&descrFifoTX, &c);
+                PLIB_USART_TransmitterByteSend(USART_ID_1, c);
+                LED5_W = !LED5_R; // Toggle Led5
+                i_cts = RS232_CTS;
+                TXsize = GetReadSize (&descrFifoTX);
+                TxBuffFull = PLIB_USART_TransmitterBufferIsFull(USART_ID_1);
+            } while ( (i_cts == 0) && ( TXsize > 0 ) && TxBuffFull==false );
+                
+		
+        // disable TX interrupt (pour éviter une interrupt. inutile si plus rien à transmettre)
+        PLIB_INT_SourceDisable(INT_ID_0, INT_SOURCE_USART_1_TRANSMIT);
+       }
+        // Clear the TX interrupt Flag (Seulement apres TX) 
+        PLIB_INT_SourceFlagClear(INT_ID_0, INT_SOURCE_USART_1_TRANSMIT);
+    }
+    // Marque fin interruption avec Led3
+    LED3_W = 0;
+ }
---
+++ app.c
@@ -0,0 +1,282 @@
+/*******************************************************************************
+  MPLAB Harmony Application Source File
+  
+  Company:
+    Microchip Technology Inc.
+  
+  File Name:
+    app.c
+
+  Summary:
+    This file contains the source code for the MPLAB Harmony application.
+
+  Description:
+    This file contains the source code for the MPLAB Harmony application.  It 
+    implements the logic of the application's state machine and it may call 
+    API routines of other MPLAB Harmony modules in the system, such as drivers,
+    system services, and middleware.  However, it does not call any of the
+    system interfaces (such as the "Initialize" and "Tasks" functions) of any of
+    the modules in the system or make any assumptions about when those functions
+    are called.  That is the responsibility of the configuration-specific system
+    files.
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2013-2014 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files 
+// *****************************************************************************
+// *****************************************************************************
+
+#include "app.h"
+#include  "GestPWM.h"
+#include  "Mc32DriverLcd.h"
+#include  "Mc32Delays.h"
+#include "Mc32gest_RS232.h"
+#include "GestPWM.h"
+#include <stdint.h>
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Global Data Definitions
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* Application Data
+
+  Summary:
+    Holds application data
+
+  Description:
+    This structure holds the application's data.
+
+  Remarks:
+    This structure should be initialized by the APP_Initialize function.
+    
+    Application strings and buffers are be defined outside this structure.
+*/
+
+APP_DATA appData;
+
+
+uint8_t LedOffFlag = 1;
+uint8_t chenillard = 0b00000001;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Callback Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/* TODO:  Add any necessary callback functions.
+*/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Local Functions
+// *****************************************************************************
+// *****************************************************************************
+
+
+/* TODO:  Add any necessary local functions.
+*/
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Initialization and State Machine Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/*******************************************************************************
+  Function:
+    void APP_Initialize ( void )
+
+  Remarks:
+    See prototype in app.h.
+ */
+
+void APP_Initialize ( void )
+{
+    /* Place the App state machine in its initial state. */
+    APP_UpdateState(APP_STATE_INIT);
+
+    
+    /* TODO: Initialize your application's state machine and other
+     * parameters.
+     */
+}
+
+
+/******************************************************************************
+  Function:
+    void APP_Tasks ( void )
+
+  Remarks:
+    See prototype in app.h.
+ */
+
+void APP_Tasks ( void )
+{
+     uint8_t CommStatus;
+
+    /* Check the application's current state. */
+    switch ( appData.state )
+    {
+        /* Application's initial state. */
+        case APP_STATE_INIT:
+        {
+            /* Initialisation Displaying */
+            lcd_init(); 
+            printf_lcd("Local settings");
+            lcd_gotoxy(1,2);
+            printf_lcd("TP2 PWM 2022-2023");
+            lcd_gotoxy(1,3);
+            printf_lcd("Joao Marques"); 
+            lcd_gotoxy(1,4);
+            printf_lcd("Caroline Mieville"); 
+            lcd_bl_on();
+            
+            
+            /*Initialisation des OC*/
+             /* Initialisations des périphériques */
+            GPWM_Initialize (&PwmData);
+            
+            // Initialisation de l'ADC
+            BSP_InitADC10 ();
+            
+            //Initialisation de UART (USART_ID_1)
+            DRV_USART0_Initialize();
+            
+            // Initialisation de la communication sérielle           
+            InitFifoComm();
+                    
+            /* Mettre à jour l'état */
+            APP_UpdateState (APP_STATE_WAIT);
+            
+            /* Toutes les LED allumées */
+            APP_LedMask ( 0x00 );
+            
+            break;
+        }
+        case APP_STATE_WAIT:
+        {
+            
+            break;
+        }
+        
+        case APP_STATE_SERVICE_TASKS:
+        { 
+
+            //reception param. remote
+            CommStatus = GetMessage(&PwmData);
+            //lecture pot.
+            if (CommStatus == 0)//local?
+            {
+                GPWM_GetSettings(&PwmData);//local
+            }
+            else 
+            {
+                GPWM_GetSettings(&PwmDataToSend);//remote
+            }
+            //affichage
+            GPWM_DispSettings(&PwmData,CommStatus);
+            //execution PWM et gestion moteur
+            GPWM_ExecPWM(&PwmData);
+            //envoi valeurs
+            if (CommStatus == 0) //local?
+            {
+                SendMessage(&PwmData); //local
+            }
+            else 
+            {
+                SendMessage(&PwmDataToSend); //remote
+            }
+            appData.state = APP_STATE_WAIT;
+            break;
+        }
+
+        /* TODO: implement your application state machine.*/
+        
+
+        /* The default state should never be executed. */
+        default:
+        {
+            /* TODO: Handle error in application's state machine. */
+            break;
+        }
+    }
+}
+
+
+
+void APP_UpdateState(APP_STATES newState)
+{
+    appData.state = newState;
+}
+
+void APP_LedMask(uint8_t LedVal)
+{
+    if((LedVal&0b00000001) != 0)
+        BSP_LEDOn(BSP_LED_0);
+    else
+       BSP_LEDOff(BSP_LED_0); 
+    if((LedVal&0b00000010)>>1 != 0)
+        BSP_LEDOn(BSP_LED_1);
+    else
+       BSP_LEDOff(BSP_LED_1); 
+    if((LedVal&0b00000100)>>2 != 0)
+        BSP_LEDOn(BSP_LED_2);
+    else
+       BSP_LEDOff(BSP_LED_2); 
+    if((LedVal&0b00001000)>>3 != 0)
+        BSP_LEDOn(BSP_LED_3);
+    else
+       BSP_LEDOff(BSP_LED_3); 
+    if((LedVal&0b00010000)>>4 != 0)
+        BSP_LEDOn(BSP_LED_4);
+    else
+       BSP_LEDOff(BSP_LED_4);    
+    if((LedVal&0b00100000)>>5 != 0)
+        BSP_LEDOn(BSP_LED_5);
+    else
+       BSP_LEDOff(BSP_LED_5);  
+    if((LedVal&0b01000000)>>6 != 0)
+        BSP_LEDOn(BSP_LED_6);
+    else
+       BSP_LEDOff(BSP_LED_6);   
+    if((LedVal&0b10000000)>>7 != 0)
+        BSP_LEDOn(BSP_LED_7);
+    else
+       BSP_LEDOff(BSP_LED_7); 
+}
+
+/*******************************************************************************
+ End of File
+ */
